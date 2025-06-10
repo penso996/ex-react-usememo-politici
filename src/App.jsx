e3eba2698dd422367c4c527dd81d517d18ed779f
penso996 <penso.andrea@outlook.it>
2025-06-10 13:19:32 +0200

    added input

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { useState, useEffect } from "react";
 function App() {
 
   const [politician, setPoliticians] = useState([]);
+  const [filter, setFilter] = useState("");
 
+  // function to fetch politicians data
   async function fetchData() {
     try {
       const response = await fetch('http://localhost:3333/politicians');
@@ -15,14 +17,27 @@ function App() {
     }
   }
 
+  // useEffect to fetch
   useEffect(() => {
     fetchData();
   }, []);
+
+  // function to handle filter change
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
 
 
   // RENDER
   return (
     <>
+      <input
+        type="text"
+        name="filter"
+        value={filter}
+        onChange={handleFilterChange}
+      />
+
       {politician.map(politician => {
         return (
           <div className="politician" key={politician.id}>
