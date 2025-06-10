c9b8085bbc20157609ba8dfb7e83116dfdc4217d
penso996 <penso.andrea@outlook.it>
2025-06-10 12:28:18 +0200

    fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
+import { useState, useEffect } from "react";
+
 function App() {
+
+  const [politician, setPoliticians] = useState([]);
+
+  async function fetchData() {
+    try {
+      const response = await fetch('http://localhost:3333/politicians');
+      const data = await response.json();
+      setPoliticians(data);
+      console.log('Dati ricevuti:', data);
+    } catch (error) {
+      console.error('Errore:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   // RENDER
   return (
